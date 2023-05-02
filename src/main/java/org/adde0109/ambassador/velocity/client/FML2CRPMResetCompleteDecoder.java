f1b1cc463cedd277fdb88e57d0d797792944fd0f
Adrian Bergqvist <adde0109@gmail.com>
2023-05-02 16:28:41 +0200

    Revert "Make minecraft packet instead of forge when decoding CRPM reset."
    This reverts commit 30e90556ff75d36a9eeb621ea7868e5bd30f1b8d.

--- src/main/java/org/adde0109/ambassador/velocity/client/FML2CRPMResetCompleteDecoder.java
+++ src/main/java/org/adde0109/ambassador/velocity/client/FML2CRPMResetCompleteDecoder.java
@@ -27,7 +27,7 @@ public class FML2CRPMResetCompleteDecoder extends ChannelInboundHandlerAdapter {
           boolean success = buf.readBoolean();
           if (id == 98) {
             try {
-              MinecraftPacket packet = new LoginPluginResponse(id, success, buf.readRetainedSlice(buf.readableBytes()));
+              IForgeLoginWrapperPacket packet = new GenericForgeLoginWrapperPacket(buf, id);
               ctx.fireChannelRead(packet);
             } finally {
               buf.release();
